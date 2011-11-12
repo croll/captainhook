2d31a948a4480abe956c49c0247c255c076ea22f
nicolas <nicolas@sengena.com>
2011-11-12 03:50:55 +0100

    field support.

--- mod/field/Main.php
+++ mod/field/Main.php
@@ -4,10 +4,160 @@ namespace mod\field;
 
 class Main {
 
-  public static function hook_core_init_http($hookname, $userdata) {
+	private static $curform=null;
+	private static $curfield=null;
+
+	public static function smartyBlock_FieldForm($params, $content, $smarty, &$repeat) {
+		if ($content === NULL) { // open tag
+			if (self::$curform !== NULL) throw new \Exception("FieldForm don't support nested FieldForms");
+			self::$curform=new FieldForm();
+		} else { // close tag
+			self::$curform=NULL;
+			return "<form method='POST' action='SITEURL/fieldform/".$params['hook_on_post']."'>".$content."</form>";
+		}
   }
 
-  public function smartyFunction_hook($params, $template) {
+  public static function smartyBlock_Field($params, $content, $smarty, &$repeat) {
+		if ($content === NULL) { // open tag
+			if (self::$curfield !== NULL) throw new \Exception("Field don't support nested Fields (but I have to support it for radios, select, ...)");
+			$classname=$params['phpclass'];
+			$field = new $classname($params['name'], $params['value']);
+			self::$curfield=$field;
+			if (self::$curform !== NULL) self::$curform->addField($field);
+		} else { // close tag
+			$field=self::$curfield;
+			self::$curfield=NULL;
+			return $field->render();
+		}
+  }
+
+  public static function smartyFunction_FieldValidation($params, $template) {
+		if (self::$curfield === NULL) throw new \Exception("FieldVerification must be inside a Field block");
+		self::$curfield->addVerification($params['regexp'], $params['message']);
   }
 
 }
+
+class FieldForm {
+	private $fields=array();
+	
+	public function addField($field) {
+		$this->fields[]=$field;
+	}
+
+	public function sqlinsert($table) {
+		$vals=array();
+		$a='';
+		$b='';
+		foreach($this->fields as $field) {
+			$a.=($a == '' ? '' : ',').'`'.$field->name.'`';
+			$b.=($b == '' ? '' : ',').'?';
+			$vals[]=$field->getValue();
+		}
+
+		$q="INSERT INTO `$table` ($a) ($b)";
+		\core\Core::$db->query($q, $vals);
+
+		return \core\Core::$db->Insert_ID();
+	}
+
+	public function sqlupdate($table, $id) {
+		$vals=array();
+		$a='';
+		foreach($this->fields as $field) {
+			$a.=($a == '' ? '' : ',').'`'.$field->name.'`=?';
+			$vals[]=$field->getValue();
+		}
+
+		$q="UPDATE `$table` SET $a WHERE `id`=?";
+		$vals[]=$id;
+		\core\Core::$db->query($q, $vals);
+
+		return \core\Core::$db->Insert_ID();
+	}
+}
+
+class Verification {
+	public $regexp;
+	public $message;
+	public $fatal;
+	public function __construct($regexp, $message, $fatal=true) {
+		$this->regexp=$regexp;
+		$this->message=$message;
+	}
+}
+
+class Element {
+	public $name;
+	public $value;
+	private $tests=array();
+
+	public function __construct($name, $default='') {
+		$this->name=$name;
+		$this->value=$default;
+	}
+
+	public function addVerification($regexp, $message) {
+		$this->tests[]=new Verification($regexp, $message);
+	}
+
+	public function test($value) {
+		$result=array();
+		foreach($this->tests as $test) {
+			if (preg_match($test->regexp, $value)) $result[]=$test->message;
+		}
+		return $result;
+	}
+
+	public function getValue() {
+		return $_REQUEST[$this->name];
+	}
+}
+
+class Text extends Element {
+	public function render() {
+		return sprintf("<input type='text' name='%s' value='%s'/>",
+									 $this->name, $this->value
+									 );
+	}
+}
+
+class RadioGroup extends Element {
+	private $radios=array();
+	
+	public function addRadio($radio) {
+		$this->radios[]=$radio;
+	}
+
+	public function render() {
+	}
+}
+
+class Radio extends Element {
+	public function render() {
+		return sprintf("<input type='radio' name='%s' value='%s'/>",
+									 $this->name, $this->value
+									 );
+	}
+}
+
+class Checkbox extends Element {
+}
+
+class Password extends Element {
+}
+
+class Textarea extends Element {
+}
+
+class Select extends Element {
+}
+
+class Submit extends Element {
+	public function render() {
+		return sprintf("<input type='submit' name='%s' value='%s'/>",
+									 $this->name, $this->value
+									 );
+	}
+}
+
--- mod/site_test/hooks_templates/content.tpl
+++ mod/site_test/hooks_templates/content.tpl
@@ -1,3 +1,29 @@
 <div style="width: 100%; height: 300px; text-align: center; border: solid 1px black; background: #aaa">
 	{hook mod='webpage' name='body'}
+
+{FieldForm hook_on_post='mod_exemple_person'}
+
+	<div> Firstname:
+	  {Field phpclass='\mod\field\Text' name='firstname' value=''}
+      	     {FieldValidation regexp='^$' message='Ce champ est obligatoire'}
+      	     {FieldValidation regexp='^[a-zA-Z]*$' message='Doit contenir uniquement des lettres'}
+      	     {FieldValidation regexp='^.{2,30}$' message='Doit faire entre 2 et 30 caractères'}
+	  {/Field}
+	</div>
+
+	<div> Lastname:
+	  {Field phpclass='\mod\field\Text' name='lastname' value=''}
+      	     {FieldValidation regexp='^$' message='Ce champ est obligatoire'}
+      	     {FieldValidation regexp='^[a-zA-Z]*$' message='Doit contenir uniquement des lettres'}
+      	     {FieldValidation regexp='^.{2,30}$' message='Doit faire entre 2 et 30 caractères'}
+	  {/Field}
+	</div>
+
+	<div>
+	  {Field phpclass='\mod\field\Submit' name='submit' value='Envoi la sauce !'}{/Field}
+	</div>
+
+{/FieldForm}
+
+
 </div>
