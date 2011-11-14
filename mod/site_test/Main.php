75cec8c55e7e729b9a23f9c19bd74edcfe5d86a5
nicolas <nicolas@sengena.com>
2011-11-14 01:47:43 +0100

    part of field commited to merge with latest beve commits

--- mod/site_test/Main.php
+++ mod/site_test/Main.php
@@ -4,22 +4,26 @@ namespace mod\site_test;
 
 class Main {
 
-	private static $urlmatches;
-
 	public static function hook_mod_site_test($hookname, $userdata, $urlmatches) {
-		self::$urlmatches=$urlmatches;
 		$page = new \mod\webpage\Main();
     $page->setLayout('mod/site_test/templates/welcome.tpl');
 		$page->display();
 	}
 
 	public static function hook_mod_site_test_tests($hookname, $userdata, $urlmatches) {
-		self::$urlmatches=$urlmatches;
+		$form = new \mod\field\FieldForm('site_test_myform', 'mod/site_test/templates/myform.tpl');
 		$page = new \mod\webpage\Main();
-		switch($urlmatches[1]) {
-		case 'field': $page->setLayout('mod/site_test/templates/field.tpl'); break;
+		$page->setLayout('mod/site_test/templates/field.tpl');
+		if ($form->isPosted() && $form->isValid()) {
+			$page->smarty->assign('site_test_firstname', $form->get_value('firstname'));
+			$page->smarty->assign('site_test_lastname', $form->get_value('lastname'));
+			$page->display();
+		} else {
+			$page = new \mod\webpage\Main();
+			$page->setLayout('mod/site_test/templates/field.tpl');
+			$page->smarty->assign('site_test_myform', $form->get_html());
+			$page->display();
 		}
-		$page->display();
 	}
 
 	public static function hook_mod_smarty_blockstart_mod_site_test_content($hookname, $userdata) {
--- mod/site_test/templates/field.tpl
+++ mod/site_test/templates/field.tpl
@@ -1,27 +1,9 @@
 {extends tplextends('webpage', 'webpage_main')}
 {block name='webpage_body'}
-{FieldForm hook_on_post='mod_exemple_person'}
-
-	<div> Firstname:
-	  {Field phpclass='\mod\field\Text' name='firstname' value=''}
-      	     {FieldValidation regexp='^$' message='Ce champ est obligatoire'}
-      	     {FieldValidation regexp='^[a-zA-Z]*$' message='Doit contenir uniquement des lettres'}
-      	     {FieldValidation regexp='^.{2,30}$' message='Doit faire entre 2 et 30 caractères'}
-	  {/Field}
-	</div>
-
-	<div> Lastname:
-	  {Field phpclass='\mod\field\Text' name='lastname' value=''}
-      	     {FieldValidation regexp='^$' message='Ce champ est obligatoire'}
-      	     {FieldValidation regexp='^[a-zA-Z]*$' message='Doit contenir uniquement des lettres'}
-      	     {FieldValidation regexp='^.{2,30}$' message='Doit faire entre 2 et 30 caractères'}
-	  {/Field}
-	</div>
-
-	<div>
-	  {Field phpclass='\mod\field\Submit' name='submit' value='Envoi la sauce !'}{/Field}
-	</div>
-
-{/FieldForm}
+ {if $site_test_myform}
+  {$site_test_myform}
+ {else}
+  <p>firstname: {$site_test_firstname|escape}</p>
+  <p>lastname: {$site_test_lastname|escape}</p>
+ {/if}
 {/block}
-
