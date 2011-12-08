fcd424ab1d457a2f6972c1e0dff8ee8d6f5f3930
niclone <niclone@croll.fr>
2011-12-08 20:27:12 +0100

    allow options

--- mod/wkhtmltopdf/Main.php
+++ mod/wkhtmltopdf/Main.php
@@ -7,13 +7,13 @@ class Main {
    * @param $returntype: 'file' will return a filename from temporary directory, you should then erase it when done
    *                     'content' will return the pdf content
    */
-  public static function htmltopdf($html, $returntype = 'file') {
+  public static function htmltopdf($html, $returntype = 'file', $options='') {
    	$bin=dirname(__FILE__).'/bin/wkhtmltopdf-i386';
     $htmlfilename=tempnam('/tmp', 'htmltopdf-html-').'.html';
     $pdffilename=tempnam('/tmp', 'htmltopdf-pdf-').'.pdf';
     file_put_contents($htmlfilename, $html);
 
-    exec($bin.' '.$htmlfilename.' '.$pdffilename);
+    exec($bin.' '.$options.' '.$htmlfilename.' '.$pdffilename);
     
     if ($returntype == 'file') return $pdffilename;
     else if ($returntype == 'content') {
