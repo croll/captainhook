22d88c9de8355a895018d6a57fd1a0f6b88a7e12
niclone <niclone@croll.fr>
2011-12-08 22:18:53 +0100

    fix on chpaginator

--- mod/cssjs/js/chpaginator.js
+++ mod/cssjs/js/chpaginator.js
@@ -11,7 +11,7 @@ var CHPaginator =new Class({
 				this.elem_num=elem_num;
 				this.rowsperpage=10;
 				this.rowscount=0;
-				this.page=1;
+				this.page=0;
 
 				this.elem_prev.set('style', 'cursor:pointer;'+this.elem_prev.get('style'));
 				this.elem_next.set('style', 'cursor:pointer;'+this.elem_prev.get('style'));
@@ -35,13 +35,14 @@ var CHPaginator =new Class({
 				this.updateDisplay();
 		},
 
+		// la page 0 est la premiere page (affichée 1)
 		setCurPage: function(page) {
 				var pagescount = Math.floor((this.rowscount-1) / this.rowsperpage) +1;
-				if ((page >= 1) && (page <= pagescount) && (page != this.page)) {
+				if ((page >= 0) && (page <= pagescount) && (page != this.page)) {
 						this.page=page;
 						this.fireEvent('page', {
 								'page': page,
-								'startrow': (page-1)*this.rowsperpage,
+								'startrow': page*this.rowsperpage,
 								'rowsperpage': this.rowsperpage
 						});
 						this.updateDisplay();
@@ -53,27 +54,33 @@ var CHPaginator =new Class({
 		updateDisplay: function() {
 				var pagescount = Math.floor((this.rowscount-1) / this.rowsperpage) +1;
 
-				if (this.page > 1) this.elem_prev.removeClass('disabled');
+				if (this.page > 0) this.elem_prev.removeClass('disabled');
 				else this.elem_prev.addClass('disabled');
 
-				if (this.page < pagescount) this.elem_next.removeClass('disabled');
+				if (this.page < (pagescount-1)) this.elem_next.removeClass('disabled');
 				else this.elem_next.addClass('disabled');
 
 				this.elem_nums.empty();
-				if (this.page > 4) {
+
+				// les pages 0 a page-1
+				if (this.page > 5) {
 						for (var i=0; i<3; i++) this.displayNum(i);
 						this.elem_nums.adopt(new Element('span', { text: ' ... ' }));
-						for (var i=(this.page-3); i<=this.page; i++) this.displayNum(i);
+						for (var i=(this.page-2); i<this.page; i++) this.displayNum(i);
 				} else {
-						for (var i=0; i<=this.page; i++) this.displayNum(i);
+						for (var i=0; i<this.page; i++) this.displayNum(i);
 				}
 
-				if (this.page < (pagescount - 4)) {
-						for (var i=(this.page+1); i<(this.page+2); i++) this.displayNum(i);
+				// la page
+				if (this.rowscount > 0) this.displayNum(this.page);
+
+				// les pages page+1 a pagescount
+				if ((this.page+1) < (pagescount - 5)) {
+						for (var i=(this.page+1); i<(this.page+3); i++) this.displayNum(i);
 						this.elem_nums.adopt(new Element('span', { text: ' ... ' }));
 						for (var i=(pagescount-3); i<pagescount; i++) this.displayNum(i);
 				} else {
-						for (var i=this.page; i<pagescount; i++) this.displayNum(i);
+						for (var i=(this.page+1); i<pagescount; i++) this.displayNum(i);
 				}
 		},
 
@@ -82,11 +89,11 @@ var CHPaginator =new Class({
 				var elem_num = this.elem_num.clone();
 				elem_num.set('text', i+1);
 				elem_num.addClass('chpaginator_num');
-				if ((this.page-1) == i) elem_num.addClass('selected');
+				if (this.page == i) elem_num.addClass('selected');
 				elem_num.set('style', 'cursor:pointer;'+elem_num.get('style'));
 				elem_num.set('style', 'cursor:pointer;'+elem_num.get('style'));
 				elem_num.addEvent('click', function(e) {
-						var page=parseInt(e.target.get('text'));
+						var page=parseInt(e.target.get('text')) - 1;
 						me.setCurPage(page);
 				});
 				this.elem_nums.adopt(elem_num);
