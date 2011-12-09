84dfb350cc2fc4ca9d26cb58fbc732e0b833e305
root <root@beve.org>
2011-12-09 05:50:10 +0100

    New message class

--- mod/cssjs/js/message.js
+++ mod/cssjs/js/message.js
@@ -1,64 +1,32 @@
 CaptainHook.Message = {
-		show: function(msg, messageType, wait) {
-			var container = document.id('message');
-			var w;
-			var self = this;
-			if (!wait && wait != 0) w = 3000;
-				else w = wait;
-			
-			if (typeOf(container) != 'element') {
-				alert("Le div 'message' n'existe pas");
-				return false;
-			}
-			var mClass;
-			switch(messageType || null) {
-			  case 'WARNING':
-					mClass='.messageWarning';	
-					break;
-			  case 'ERROR':
-					mClass='.messageError';	
-					break;
+		show: function(txt, messageType, options) {
+		console.log(txt);
+			var icon;
+			switch(messageType) {
+				case 'OK':
+					icon = 'okMedium.png';
+				case 'ERROR':
+					icon = 'errorMedium.png';
 				default:
-					mClass='.messageOk';	
-					break;
+					icon = 'cautionMedium.png';
 			}
-			container.empty().set('html', msg);
-			container.erase('class').addClass(mClass);
-			
-			var myEffect = new Fx.Morph(container, {
-					duration: 500,
-					link: 'chain',
-					transition: Fx.Transitions.Sine.easeOut
-			});
-			var myEffectHide = new Fx.Morph(container, {
-					duration: 500,
-					link: 'chain',
-					transition: Fx.Transitions.Sine.easeOut,
-					onComplete: function() {
-						self.hide.delay(w);
-					}
-			});
-			if (wait == 0)
-				myEffect.start(mClass);
-			else
-				myEffectHide.start(mClass);
-		}, 
 
-		hide: function() {
-			var container = document.id('message');
-			var myEffect = new Fx.Morph(container, {
-					duration: 500,
-					link: 'chain',
-					transition: Fx.Transitions.Sine.easeOut,
-					onComplete: function() {
-						container.empty().set('html', '');
-					}
-			});
-			myEffect.start('.messageHide');
-		}
+			var msg = new Message(Object.merge({
+				iconPath: '/mod/cssjs/images/message/',
+				icon: icon,
+				title: 'Information',
+				message: txt,
+				offset: 150,
+				centered: true,
+				top: true
+			}, options)).say();
+		} 
+
 }
 
 window.addEvent('domready', function(){
-	if ($('message') && $('message').get('html') != '')
-		CaptainHook.Message.show($('message').get('html'), $('message').get('rel'), 0);
+	if ($('message') && $('message').get('html') != '') {
+		CaptainHook.Message.show($('message').get('html'), $('message').get('rel'), {width: 320});
+		$('message').set('html', '');
+	}
 });
