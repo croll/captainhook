fb59e2bb32825ebb3e48a4cb6d9e51ae5c9f29e7
Christophe Beveraggi <beve@croll.fr>
2012-03-09 17:29:07 +0100

    - Moogoomaps removed - Marker wip

--- mod/map/Marker.php
+++ mod/map/Marker.php
@@ -4,7 +4,48 @@ namespace mod\map;
 
 class Marker {
 
-	public static function get($params) {
+	private $_marker = array();
+
+	function __construct($type, $geometry) {
+		$this->_marker['type'] = $type;
+		$this->_marker['geometry'] = $geometry;
 	}
+
+	function setGeometry($geometry) {
+		$this->_marker['geometry'] = $params['geometry'];
+	}
+
+	function setIconParams($params) {
+		$filename = md5(serialize($params)).'.png';
+		$cachedImage = CH_MODDIR.'/map/cache/'.$filename;
+		if (!is_file($cachedImage)) {
+			$params['filename'] = $filename;
+			$markerImage = self::buildIcon($params);
+		} else
+			$markerImage = '/mod/map/cache/'.$filename;
+		$this->_marker['icon']['iconUrl'] = $markerImage;
+		$this->_marker['icon']['iconSize'] = $params['size'];
+	}
+
+	public function get() {
+		return $this->_marker;
+	}
+
+	function setPopupParams($params) {
+		$this->_marker['popup'] = $params;
+	}
+
+	public static function buildIcon($params) {
+		\core\Core::log($params);
+		require_once(dirname(__FILE__).'/MarkerBuilder.php');
+		$className = '\\mod\\map\\Marker'.ucfirst($params['shape']);
+		try {
+			$class = new $className($params);
+			return $class->getIconPath();
+		} catch (\Exception $e) {
+			\core\Core::log($e->getMessage());
+		}
+	}
+
 }
 ?>
--- mod/map/MarkerBuilder.php
+++ mod/map/MarkerBuilder.php
@@ -3,12 +3,48 @@
 namespace mod\map;
 
 class MarkerBuilder {
+	
+	protected $_imaObj;
+	protected $_drawObj;
+	protected $_params;
 
-	function __construct() {
-		$this->marker = (object)NULL;
+	function __construct($params) {
+		$this->_params = $params;
+		$this->_imgObj = new \Imagick();
+		$this->_imgObj->newImage($params['size'][0], $params['size'][1], "none");
+		$this->_drawObj = new \ImagickDraw();
+		$this->_drawObj->setFontSize(12);
+		$this->_drawObj->setFillColor(new \ImagickPixel($params['color']));
+
+		if ($params['strokecolor']) {
+			$this->_drawObj->setStrokeColor(new \ImagickPixel($params['strokecolor']));
+		}
+		$this->draw();
+		$this->_imgObj->drawImage($this->_drawObj);
 	}
 
-	function drawStar($x, $y, $radius, $spikes=5) {
+	function getIconPath() {
+		$this->_imgObj->setImageFormat('png');
+		$this->_imgObj->writeImage(CH_MODDIR.'/map/cache/'.$this->_params['filename']);
+		return '/mod/map/cache/'.$this->_params['filename'];
+	}
+
+	function draw() {
+	}
+
+}
+
+class MarkerCircle extends MarkerBuilder {
+
+	function draw() {
+		$this->_drawObj->circle($this->_params['size'][0]/2, $this->_params['size'][0]/2, ($this->_params['size'][1]/2)-2, $this->_params['size'][1]-2);  
+	}
+
+}
+
+class MarkerStar extends MarkerBuilder {
+
+	function draw() {
 		$coordinates = array();
 		$angel = 360 / $spikes ;
 		$outer_shape = array();
@@ -38,6 +74,7 @@ class MarkerBuilder {
 			$coordinates[] = $inner_shape[$key]['x'];
 			$coordinates[] = $inner_shape[$key]['y'];
 		}
-		return $coordinates ;
+		$this->_drawObj->polygon($coordinates);
 	}
+
 }
