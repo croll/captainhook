d6abdab8efaf1133e0634b5d5b2e19307ad1223e
tombombadilom <tom@croll.fr>
2012-03-09 19:06:48 +0100

    Module user fixed regroute conflicts new path /useredit/0 	modified:   mod/user/ModuleDefinition.php

--- mod/user/ModuleDefinition.php
+++ mod/user/ModuleDefinition.php
@@ -16,7 +16,7 @@ class ModuleDefinition extends \core\ModuleDefinition {
     parent::install();
     \mod\regroute\Main::registerRoute($this->id, '#^/login/?f?r?o?m?=?(.*)$#', 'mod_user_login');
     \mod\regroute\Main::registerRoute($this->id, '#^/logout/?$#', 'mod_user_logout');
-    \mod\regroute\Main::registerRoute($this->id, '#^/user/edit/([0-9]+)$#', 'mod_user_edit', \mod\regroute\Main::flag_html | \mod\regroute\Main::flag_xmlhttprequest);
+    \mod\regroute\Main::registerRoute($this->id, '#^/useredit/([0-9]+)$#', 'mod_user_edit', \mod\regroute\Main::flag_html | \mod\regroute\Main::flag_xmlhttprequest);
     \mod\regroute\Main::registerRoute($this->id, '#^/user/([a-z0-9/_:@]+)?$#', 'mod_user_manage_users');
 	   
   }
